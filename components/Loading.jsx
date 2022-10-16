d11e0ff74c6f736c70025ba8b5e18c2073960442
king11223344 <51441410+king11223344@users.noreply.github.com>
2022-10-16 19:21:34 +0530

    Add files via upload

---
+++ components/Loading.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { CirclesWithBar } from "react-loader-spinner";
+
+function Loading() {
+  <div className="flex justify-center items-center ">
+    {/* <CirclesWithBar
+      height="100"
+      width="100"
+      color="#4fa94d"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      outerCircleColor=""
+      innerCircleColor=""
+      barColor=""
+      ariaLabel="circles-with-bar-loading"
+    /> */}
+    <h1>Loading...</h1>
+    <h2>yuguytyut</h2>
+  </div>;
+}
+export default Loading;
